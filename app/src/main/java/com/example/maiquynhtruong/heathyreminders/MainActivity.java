f72a988f483319635a4c0aaf3198ed60a05c697c
Mai Truong <maitruong@gmail.com>
2017-07-25 14:16:02 -0400

    commit for today 7/25/2017

--- app/src/main/java/com/example/maiquynhtruong/heathyreminders/MainActivity.java
+++ app/src/main/java/com/example/maiquynhtruong/heathyreminders/MainActivity.java
@@ -190,17 +190,18 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_about) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_finished) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_recommendations) {
+            Intent recommendationIntent = new Intent();
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_reminders) {
+
+        } else if (id == R.id.nav_settings) {
 
         }
 
