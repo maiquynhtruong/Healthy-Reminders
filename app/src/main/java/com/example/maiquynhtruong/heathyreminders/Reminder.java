36acd3f50122b291fd6776e32884cd5fa5b3c8cf
Mai Truong <maitruong@gmail.com>
2017-07-25 14:28:16 -0400

    another commit

---
+++ app/src/main/java/com/example/maiquynhtruong/heathyreminders/Reminder.java
@@ -0,0 +1,14 @@
+package com.example.maiquynhtruong.heathyreminders;
+
+import java.util.Date;
+
+enum ReminderMethod {
+    EMAIL, NOTIFICATION, ALARM
+}
+public class Reminder {
+    String name;
+    // occurrences
+    ReminderMethod method = ReminderMethod.NOTIFICATION;
+    // Constraints
+
+}
