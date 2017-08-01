550ecd629636e5ad1d33de04fa12bba09a5897d2
Mai Truong <maitruong@gmail.com>
2017-08-01 11:41:48 -0400

    add reminder fragments

--- app/src/main/java/com/example/maiquynhtruong/heathyreminders/MainActivity.java
+++ app/src/main/java/com/example/maiquynhtruong/heathyreminders/MainActivity.java
@@ -58,14 +58,14 @@ public class MainActivity extends AppCompatActivity
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
         setUpNavigationDrawer();
         mainRecyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
