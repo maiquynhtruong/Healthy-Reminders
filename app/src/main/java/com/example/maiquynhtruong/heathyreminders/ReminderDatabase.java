93764aab7b13ea4ece5a3ff6bfd8bfef32e230f2
Mai Truong <maitruong2801@gmail.com>
2017-08-06 23:52:47 -0400

    move all database operations to database class because I just realized I should not put them in the service class

--- app/src/main/java/com/example/maiquynhtruong/heathyreminders/ReminderDatabase.java
+++ app/src/main/java/com/example/maiquynhtruong/heathyreminders/ReminderDatabase.java
@@ -1,11 +1,14 @@
 package com.example.maiquynhtruong.heathyreminders;
 
+import android.app.AlarmManager;
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteCursorDriver;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQuery;
+import android.icu.util.Calendar;
 import android.provider.BaseColumns;
 
 import static com.example.maiquynhtruong.heathyreminders.ReminderDatabase.ReminderEntry.REMINDER_DATE;
@@ -40,6 +43,48 @@ public class ReminderDatabase extends SQLiteOpenHelper {
         sqLiteDatabase.execSQL(SQL_DELETE_ENTRIES);
         onCreate(sqLiteDatabase);
     }
+    public long setReminder(Reminder reminder) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(ReminderEntry.REMINDER_TITLE, reminder.getTitle());
+        values.put(ReminderEntry.REMINDER_DATE, reminder.getDate());
+        values.put(ReminderEntry.REMINDER_HOUR, reminder.getHour());
+        values.put(ReminderEntry.REMINDER_MINUTE, reminder.getMinute());
+        values.put(ReminderEntry.REMINDER_REPEAT, reminder.isRepeat());
+        values.put(ReminderEntry.REMINDER_REPEAT_NUMBER, reminder.getRepeatNumber());
+        long newRowId = database.insert(ReminderDatabase.ReminderEntry.TABLE_NAME, null, values);
+        return newRowId;
+    }
+
+    public Reminder getReminder(long id) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        Cursor cursor = database.query(ReminderDatabase.ReminderEntry.TABLE_NAME,
+                new String[]{
+                        ReminderEntry.REMINDER_TITLE,
+                        ReminderEntry.REMINDER_DATE,
+                        ReminderEntry.REMINDER_HOUR,
+                        ReminderEntry.REMINDER_MINUTE,
+                        ReminderEntry.REMINDER_REPEAT_NUMBER,
+                        ReminderEntry.REMINDER_REPEAT,
+                }, ReminderDatabase.ReminderEntry.REMINDER_ID + " = ?",
+                new String[]{"" + id}, null, null, null);
+        Reminder reminder = new Reminder("Default reminder");
+        if (cursor.moveToFirst()) {
+            reminder.setTitle(cursor.getString(1));
+            reminder.setDate(cursor.getString(2));
+            reminder.setHour(Integer.parseInt(cursor.getString(3)));
+            reminder.setMinute(Integer.parseInt(cursor.getString(4)));
+            reminder.setRepeat(Boolean.parseBoolean(cursor.getString(5)));
+            reminder.setRepeatNumber(Integer.parseInt(cursor.getString(6)));
+        }
+        return reminder;
+    }
+
+    public void deleteReminder(long id) {
+        SQLiteDatabase database = this.getWritableDatabase();
+        database.delete(TABLE_NAME, ReminderEntry.REMINDER_ID + " = ?", new String[] {"" + id});
+    }
+
     public static class ReminderEntry implements BaseColumns {
         public static final String TABLE_NAME = "ReminderEntries";
         public static final String REMINDER_ID = "id";
--- app/src/main/java/com/example/maiquynhtruong/heathyreminders/Services/AlarmService.java
+++ app/src/main/java/com/example/maiquynhtruong/heathyreminders/Services/AlarmService.java
@@ -34,57 +34,24 @@ public class AlarmService extends IntentService {
         String action = intent.getAction();
     }
 
-    public static void setReminder(Context context, Reminder reminder, Calendar calendar) {
-        AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
 
-        ContentValues values = new ContentValues();
-        values.put(ReminderDatabase.ReminderEntry.REMINDER_TITLE, reminder.getTitle());
-        values.put(ReminderDatabase.ReminderEntry.REMINDER_DATE, reminder.getDate());
-        values.put(ReminderDatabase.ReminderEntry.REMINDER_HOUR, reminder.getHour());
-        values.put(ReminderDatabase.ReminderEntry.REMINDER_MINUTE, reminder.getMinute());
-        values.put(ReminderDatabase.ReminderEntry.REMINDER_REPEAT, reminder.isRepeat());
-        values.put(ReminderDatabase.ReminderEntry.REMINDER_REPEAT_NUMBER, reminder.getRepeatNumber());
 
-        long newRowId = ReminderApplication.database.insert(ReminderDatabase.ReminderEntry.TABLE_NAME, null, values);
+//        Intent intent = new Intent(context, AlarmReceiver.class);
+//        // Retrieve a PendingIntent that will perform a broadcast, like calling Context.sendBroadcast()
+//        // sendBroadcast() will broadcast the Intent; all receivers matching this Intent will receive the broadcast
+//        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+//        // With setInexactRepeating(), you have to use one of the AlarmManager interval
+//        // constants--in this case, AlarmManager.INTERVAL_DAY.
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//            manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
+//        }
+//    }
 
-        Intent intent = new Intent(context, AlarmReceiver.class);
-        // Retrieve a PendingIntent that will perform a broadcast, like calling Context.sendBroadcast()
-        // sendBroadcast() will broadcast the Intent; all receivers matching this Intent will receive the broadcast
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        // With setInexactRepeating(), you have to use one of the AlarmManager interval
-        // constants--in this case, AlarmManager.INTERVAL_DAY.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            manager.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
-        }
-    }
-
-    public static Reminder getReminder(long id) {
-        Cursor cursor = ReminderApplication.database.query(ReminderDatabase.ReminderEntry.TABLE_NAME,
-                new String[] {
-                        ReminderDatabase.ReminderEntry.REMINDER_TITLE,
-                        ReminderDatabase.ReminderEntry.REMINDER_DATE,
-                        ReminderDatabase.ReminderEntry.REMINDER_HOUR,
-                        ReminderDatabase.ReminderEntry.REMINDER_MINUTE,
-                        ReminderDatabase.ReminderEntry.REMINDER_REPEAT,
-                        ReminderDatabase.ReminderEntry.REMINDER_REPEAT_NUMBER,
-                }, ReminderDatabase.ReminderEntry.REMINDER_ID + " = ?",
-                new String[] {"" + id}, null, null, null);
-        Reminder reminder = new Reminder("Default reminder");
-        if (cursor.moveToFirst()) {
-            reminder.setTitle(cursor.getString(1));
-            reminder.setDate(cursor.getString(2));
-            reminder.setHour(Integer.parseInt(cursor.getString(3)));
-            reminder.setMinute(Integer.parseInt(cursor.getString(4)));
-            reminder.setRepeat(Boolean.parseBoolean(cursor.getString(5)));
-            reminder.setRepeatNumber(Integer.parseInt(cursor.getString(6)));
-        }
-        return reminder;
-    }
 
     public List<Reminder> getAllReminders() {
         List<Reminder> reminderList = new ArrayList<Reminder>();
     }
-    }
+
     public void cancelReminder(Context context, int requestCode) {
         AlarmManager manager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, AlarmReceiver.class);
