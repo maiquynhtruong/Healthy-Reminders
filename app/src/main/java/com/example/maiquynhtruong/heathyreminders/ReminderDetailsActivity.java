74f92f6a983054c04c472b4a980a26ae0fb29a98
Mai Truong <maitruong2801@gmail.com>
2017-08-24 16:12:40 -0400

    add more code to edit reminder

--- app/src/main/java/com/example/maiquynhtruong/heathyreminders/ReminderDetailsActivity.java
+++ app/src/main/java/com/example/maiquynhtruong/heathyreminders/ReminderDetailsActivity.java
@@ -1,5 +1,6 @@
 package com.example.maiquynhtruong.heathyreminders;
 
+import android.app.AlarmManager;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
@@ -21,16 +22,15 @@ import mehdi.sakout.fancybuttons.FancyButton;
 public class ReminderDetailsActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener{
     TextInputEditText reminderTitle;
     TextView date, time;
-    int hour, minute, month, day, year, repeatNumber;
-    long reminderID;
+    int hourOfDay, minute, month, dayOfMonth, year, repeatNumber;
+    int reminderID;
     boolean repeat;
     String repeatType;
     Reminder reminder;
-    FancyButton cancelBtn, saveBtn;
+    FancyButton cancelBtn, updateBtn;
     ReminderDatabase database;
     Calendar calendar;
     ReminderReceiver receiver;
-    ReminderAdapter adapter;
     public static final String REMINDER_DETAILS_ID = "reminder-id";
     public static final int EDIT_REMINDER_REQUEST_CODE = 1;
 
@@ -42,7 +42,7 @@ public class ReminderDetailsActivity extends AppCompatActivity implements DatePi
         time = (TextView) findViewById(R.id.timePicker);
         reminderTitle = (TextInputEditText) findViewById(R.id.reminder_title);
         cancelBtn = (FancyButton) findViewById(R.id.btn_cancel);
-        saveBtn = (FancyButton) findViewById(R.id.btn_save);
+        updateBtn = (FancyButton) findViewById(R.id.btn_save);
         database = new ReminderDatabase(this);
 
 
@@ -59,8 +59,8 @@ public class ReminderDetailsActivity extends AppCompatActivity implements DatePi
             date.setText(reminder.getMonth() + "/" + reminder.getDay() + "/" + reminder.getYear());
             time.setText(reminder.getHour() + ":" + reminder.getMinute());
         }
-        saveBtn.setText("UPDATE");
-        saveBtn.setOnClickListener(new View.OnClickListener() {
+        updateBtn.setText("UPDATE");
+        updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 updateReminder(view);
@@ -77,29 +77,51 @@ public class ReminderDetailsActivity extends AppCompatActivity implements DatePi
 
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
-        calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.MONTH, month);
-        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        this.year = year;
+        this.month = month;
+        this.dayOfMonth = dayOfMonth;
     }
 
     @Override
     public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
-        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
-        calendar.set(Calendar.MINUTE, minute);
+        this.hourOfDay = hourOfDay;
+        this.minute = minute;
     }
 
     public void updateReminder(View view) {
         reminder.setTitle(reminderTitle.toString());
-        reminder.setHour(hour);
+        reminder.setHour(hourOfDay);
         reminder.setMinute(minute);
-        reminder.setDay(day);
+        reminder.setDay(dayOfMonth);
         reminder.setMonth(month);
         reminder.setYear(year);
         reminder.setRepeat(repeat);
         reminder.setRepeatNumber(repeatNumber);
         reminder.setRepeatType(repeatType);
+
+        // update reminder in database
         database.updateReminder(reminder);
-        adapter.notifyDataSetChanged();
+
+        // update calendar with new time
+        calendar.set(Calendar.YEAR, year);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        calendar.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        calendar.set(Calendar.MINUTE, minute);
+
+        // cancel old notification
+        receiver.cancelAlarm(getApplicationContext(), reminderID);
+        // create new notification
+        if (repeatType.equals(Reminder.MONTHLY) || repeatType.equals(Reminder.YEARLY)) new ReminderReceiver().setReminderMonthOrYear(this, calendar.getTimeInMillis(),
+                reminderID, repeatType);
+        else if (repeatType.equals(Reminder.HOURLY)) new ReminderReceiver().setReminderHourOrDayOrWeek(this, calendar.getTimeInMillis(),
+                reminderID, AlarmManager.INTERVAL_HOUR);
+        else if (repeatType.equals(Reminder.DAILY)) new ReminderReceiver().setReminderHourOrDayOrWeek(this, calendar.getTimeInMillis(),
+                reminderID, AlarmManager.INTERVAL_DAY);
+        else if (repeatType.equals(Reminder.WEEKLY)) new ReminderReceiver().setReminderHourOrDayOrWeek(this, calendar.getTimeInMillis(),
+                reminderID, AlarmManager.INTERVAL_DAY*7);
+
+        onBackPressed();
     }
 
     @Override
