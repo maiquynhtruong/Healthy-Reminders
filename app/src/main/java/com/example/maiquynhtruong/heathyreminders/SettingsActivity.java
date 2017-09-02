dd25c34bf7f8f553fa3286570945c4d9636264ca
Mai Truong <maitruong2801@gmail.com>
2017-09-01 22:36:36 -0400

    after more than a day I decided to just add the color property to reminder. Lets see how it goes

---
+++ app/src/main/java/com/example/maiquynhtruong/heathyreminders/SettingsActivity.java
@@ -0,0 +1,10 @@
+package com.example.maiquynhtruong.heathyreminders;
+
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by maiquynhtruong on 9/1/17.
+ */
+
+public class SettingsActivity extends PreferenceActivity {
+}
